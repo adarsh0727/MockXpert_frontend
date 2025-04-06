2cdb8ff9c3a2648c95f9deb05ff33ad05b0bc505
wthug <gdscutsav@gmail.com>
2025-04-06 23:01:26 +0530

    home page refresh thing

--- src/pages/InterviewPage.jsx
+++ src/pages/InterviewPage.jsx
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
 import { ResizablePanelGroup } from "@/components/ui/resizable";
-import ConversationPanel from '../components/interviewPage/ConservationPanel';
-import ResponsePanel from '../components/interviewPage/ResponsePanel';
-import InterviewHeader from '../components/interviewPage/InterviewHeader';
+import ConversationPanel from "../components/interviewPage/ConservationPanel";
+import ResponsePanel from "../components/interviewPage/ResponsePanel";
+import InterviewHeader from "../components/interviewPage/InterviewHeader";
 import { useInterviewStore } from "../store/useInterviewStore";
-
 
 const InterviewPage = () => {
   const [defaultLayout, setDefaultLayout] = useState([50, 50]);
-  const navigate = useNavigate();
   const {
     nextQuestionReady,
     interviewShouldEnd,
@@ -18,33 +15,29 @@ const InterviewPage = () => {
     endInterview,
   } = useInterviewStore();
 
-  const time = new Date();
-  time.setSeconds(time.getSeconds() + 15);
-  
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  // const time = new Date();
+  // time.setSeconds(time.getSeconds() + 15);
 
   return (
     <div className="h-screen flex flex-col bg-background">
-      <InterviewHeader 
+      <InterviewHeader
         generateNewQuestion={generateNewQuestion}
         endInterview={endInterview}
         nextQuestionReady={nextQuestionReady}
         interviewShouldEnd={interviewShouldEnd}
       />
-      
+
       <ResizablePanelGroup
         direction="horizontal"
         className="flex-1"
         onLayout={(sizes) => setDefaultLayout(sizes)}
       >
-        <ConversationPanel 
+        <ConversationPanel
           defaultSize={defaultLayout[0]}
           generatingResponse={generatingResponse}
         />
-        
-        <ResponsePanel 
-          defaultSize={defaultLayout[1]} 
-        />
+
+        <ResponsePanel defaultSize={defaultLayout[1]} />
       </ResizablePanelGroup>
     </div>
   );
