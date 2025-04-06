47136dcc2a3fbc727e7e132eafa6660910552825
wthug <gdscutsav@gmail.com>
2025-04-06 21:55:45 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3001/api"
-      : "/",
+      : "/api",
   withCredentials: true,
 });
